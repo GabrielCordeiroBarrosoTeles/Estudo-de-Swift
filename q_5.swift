a0d9f937f7a2e3b6aab5fe8385631a32b93a0589
Gabriel Cordeiro Barroso Teles <98492418+GabrielCordeiroBarrosoTeles@users.noreply.github.com>
2023-09-22 14:06:19 -0300

    Update and rename q_5. swift.txt to q_5.swift

--- q_5.swift
+++ q_5.swift
@@ -2,6 +2,8 @@ func celsiusParaFahrenheit(_ celsius: Double) -> Double {
     return (celsius * 9/5) + 32
 }
 
-let temperaturaCelsius = 25.0
+let temperatura
+
+Celsius = 25.0
 let temperaturaFahrenheit = celsiusParaFahrenheit(temperaturaCelsius)
 print(temperaturaFahrenheit) // Output: 77.0
